909b74f332e7e40f1b6671d98a64a795ff82cccc
Dandara Moraes <dandisbdm@gmail.com>
2025-05-17 23:51:01 -0300

    Refatoração de código, Paginação e Testes negativo e positivo

--- src/test/java/modulos/produtos/ProdutosTest.java
+++ src/test/java/modulos/produtos/ProdutosTest.java
@@ -1,54 +1,113 @@
 package modulos.produtos;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import paginas.LoginPage;
 
 import java.time.Duration;
 
 @DisplayName("Testes Web do Modulo de Produtos")
 public class ProdutosTest {
+
+    private WebDriver navegador;
+
+    @BeforeEach
+    public void beforeEach(){
+        //Abrir o navegador
+        System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver-win64\\chromedriver.exe");
+        this.navegador = new ChromeDriver();
+        
+        //Vou maximizar a tela
+        this.navegador.manage().window().maximize();
+
+        //Vou definir um tempo de espera padrao de 5 segundos
+        this.navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        // Navegar para a pagina da Lojinha Web
+        this.navegador.get("http://165.227.93.41/lojinha-web/v2");
+
+    }
     @Test
     @DisplayName("Nao e permitido regitrar um produto com valor igual a zero")
     public void testNaoEPermitoRegistrarProdutoComValorIgualAZero() {
-        //Abrir o navegador
-        System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver-win64\\chromedriver.exe");
-        WebDriver navegador = new ChromeDriver();
 
-        //Vou maximizar a tela
-        navegador.manage().window().maximize();
+        //Paginas
+        String mensagemToastApresentada = new LoginPage(navegador) //A mensagem que está no métdo "capturaMensagemApresentada" é exibida
+                //Fazer o login
+                .informarOUsuario("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                //Vou para a tela de registro de produto
+                .acessarFormularioAdicaoNovoProduto()
+                //Vou preencher dados do produto e o valor sera igual a zero
+                .informarNomeDoProduto("Tablet")
+                .informarValorDoProduto("000")
+                .informarCoresDoProduto("preto, branco, rosa")
+                //Vou submeter o formulario
+                .submeterFormularioDeAdicaoComErro()
+                //Validar mensagem
+                .capturarMensagemApresentada();
 
-        //Vou definir um tempo de espera padrao de 5 segundos
-        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemToastApresentada);
+    }
 
-        // Navegar para a pagina da Lojinha Web
-        navegador.get("http://165.227.93.41/lojinha-web-bugada/v2");
+    @Test
+    @DisplayName("Nao e permitido registrar um produto com valor acima de 7000,01")
+    public void testNaoEPermitidoRegistrarProdutoComValorAcimaDe7000(){
 
-        //Fazer o login
-        navegador.findElement(By.cssSelector("label[for='usuario']")).click();
-        navegador.findElement(By.id("usuario")).sendKeys("admin");
+        //Paginas
+        String mensagemToastApresentada = new LoginPage(navegador) //A mensagem que está no métdo "capturaMensagemApresentada" é exibida
+                //Fazer o login
+                .informarOUsuario("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                //Vou para a tela de registro de produto
+                .acessarFormularioAdicaoNovoProduto()
+                //Vou preencher dados do produto e o valor sera igual a R$7000,01
+                .informarNomeDoProduto("Xbox")
+                .informarValorDoProduto("700001")
+                .informarCoresDoProduto("preto")
+                //Vou submeter o formulario
+                .submeterFormularioDeAdicaoComErro()
+                //Validar mensagem
+                .capturarMensagemApresentada();
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemToastApresentada);
+    }
 
-        navegador.findElement(By.cssSelector("label[for='senha']")).click();
-        navegador.findElement(By.id("senha")).sendKeys("admin");
+    @Test
+    @DisplayName("Permitir Registrar Produto Dentro Do Valor Limite")
+    public void testPermitirRegistrarProdutoDentroDoValorLimite(){
 
-        navegador.findElement(By.cssSelector("button[type='submit']")).click();
+        String mensagemToastApresentada = new LoginPage(navegador) //A mensagem que está no métdo "capturaMensagemApresentada" é exibida
+                //Fazer o login
+                .informarOUsuario("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                //Vou para a tela de registro de produto
+                .acessarFormularioAdicaoNovoProduto()
+                //Vou preencher dados do produto e com valor dentro do limite
+                .informarNomeDoProduto("Notebook ABC")
+                .informarValorDoProduto("250000")
+                .informarCoresDoProduto("preto")
+                //Adicionar produto com sucesso
+                .submeterFormularioDeAdicaoComSucesso()
+                //Validar mensagem
+                .capturarMensagemApresentada();
 
-        //Vou para a tela de registro de produto
-        navegador.findElement(By.linkText("ADICIONAR PRODUTO")).click();
+        Assertions.assertEquals("Produto adicionado com sucesso",mensagemToastApresentada);
 
-        //Vou preencher dados do produto e o valor sera igual a zero
-        navegador.findElement(By.id("produtonome")).sendKeys("Tablet");
-        navegador.findElement(By.name("produtovalor")).sendKeys("140000");
-        navegador.findElement(By.id("produtocores")).sendKeys("preto, branco, rosa");
+    }
 
-        //Vou submeter o formulario
-        navegador.findElement(By.cssSelector("button[type='submit']")).click();
 
-        //Vou validar que a mensagem de erro foi apresentado
-        String mensagemToast = navegador.findElement(By.cssSelector(".toast.rounded")).getText();
-        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemToast);
+
+
+
+
+
+        @AfterEach
+        public void afterEach() {
+        //Vou fechar o navegador
+        navegador.quit();
     }
 }
