39ab964fcfa5d5d8dfcb089c6210f889bb397136
Dandara Moraes <dandisbdm@gmail.com>
2025-05-18 19:18:32 -0300

    Refatoração de código

--- src/test/java/modulos/produtos/ProdutosTest.java
+++ src/test/java/modulos/produtos/ProdutosTest.java
@@ -1,54 +1,65 @@
 package modulos.produtos;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import paginas.LoginPage;
 
 import java.time.Duration;
 
+
 @DisplayName("Testes Web do Modulo de Produtos")
 public class ProdutosTest {
+
+    private  WebDriver navegador;
+
+    @BeforeEach
+    public void beforeEach(){
+        //Abrir o navegador
+        System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver-win64\\chromedriver.exe");
+        this.navegador = new ChromeDriver();
+
+        //Vou maximizar a tela
+        this.navegador.manage().window().maximize();
+
+        //Vou definir um tempo de espera padrao de 5 segundos
+        this.navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        // Navegar para a pagina da Lojinha Web
+        this.navegador.get("http://165.227.93.41/lojinha-web/v2");
+
+    }
+
     @Test
     @DisplayName("Nao e permitido regitrar um produto com valor igual a zero")
     public void testNaoEPermitoRegistrarProdutoComValorIgualAZero() {
-        //Abrir o navegador
-        System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver-win64\\chromedriver.exe");
-        WebDriver navegador = new ChromeDriver();
 
-        //Vou maximizar a tela
-        navegador.manage().window().maximize();
-
-        //Vou definir um tempo de espera padrao de 5 segundos
-        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
-        // Navegar para a pagina da Lojinha Web
-        navegador.get("http://165.227.93.41/lojinha-web-bugada/v2");
 
         //Fazer o login
-        navegador.findElement(By.cssSelector("label[for='usuario']")).click();
-        navegador.findElement(By.id("usuario")).sendKeys("admin");
+        String mensagemToastApresentada = new LoginPage(navegador)
+                .informarOUsuario("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                //Vou para a tela de registro de produto
+                .acessarFormularioDeAdicaoNovoProduto()
+                //Vou preencher dados do produto e o valor sera igual a zero
+                .informarNomeDoProduto("Mouse sem fio")
+                .informarValorDoProduto("000")
+                .informarCoresDoProduto("branco, cinza, preto")
+                //Preenchimento do fomulario
+                .submeterFormularioDeAdicaoComErro()
+                //Validar mensagem
+                .capturarMensagemApresentada();
 
-        navegador.findElement(By.cssSelector("label[for='senha']")).click();
-        navegador.findElement(By.id("senha")).sendKeys("admin");
-
-        navegador.findElement(By.cssSelector("button[type='submit']")).click();
-
-        //Vou para a tela de registro de produto
-        navegador.findElement(By.linkText("ADICIONAR PRODUTO")).click();
-
-        //Vou preencher dados do produto e o valor sera igual a zero
-        navegador.findElement(By.id("produtonome")).sendKeys("Tablet");
-        navegador.findElement(By.name("produtovalor")).sendKeys("140000");
-        navegador.findElement(By.id("produtocores")).sendKeys("preto, branco, rosa");
-
-        //Vou submeter o formulario
-        navegador.findElement(By.cssSelector("button[type='submit']")).click();
-
-        //Vou validar que a mensagem de erro foi apresentado
-        String mensagemToast = navegador.findElement(By.cssSelector(".toast.rounded")).getText();
-        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemToast);
+        //Validar a mensagem que foi enviada para o Toast
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemToastApresentada);
     }
+
+    @AfterEach
+    public void afterEach(){
+        //Fechar o navegador
+        navegador.quit();
+    }
+
 }
